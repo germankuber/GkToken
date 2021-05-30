1a80ce7f5d0f586716b45f1edb426b532208a8e3
Germán Küber <german.kuber@outlook.com>
2021-05-30 04:54:28 -0300

    First commit

---
+++ contracts/GkToken.sol
@@ -0,0 +1,73 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Ballot
+ * @dev Implements voting process along with vote delegation
+ */
+contract GkToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    constructor(uint256 _totalSupply) {
+        name = "Gk Coint";
+        symbol = "GK";
+        decimals = 1;
+        totalSupply = _totalSupply * (uint256(1)**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(
+            balanceOf[msg.sender] >= _value,
+            "You do not have tokens to spend"
+        );
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(
+            balanceOf[_from] >= _value,
+            "You do not have enough tokens to transfer"
+        );
+        require(
+            allowance[_from][msg.sender] >= _value,
+            "From account does not have enough tokens"
+        );
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+}
---
+++ test/sample-test.js
@@ -0,0 +1,82 @@
+const { expect } = require("chai");
+
+describe("GkToken", () => {
+  let gkToken;
+  let owner;
+  let firstAccount;
+  let secondAccount;
+  before(async () => {
+    const GkToken = await ethers.getContractFactory("GkToken");
+    gkToken = await GkToken.deploy(1000);
+    await gkToken.deployed();
+    const accounts = await ethers.getSigners();
+    owner = accounts[0];
+    firstAccount = accounts[1];
+    secondAccount = accounts[2];
+    gkToken = await gkToken.connect(owner);
+
+  });
+  it("Owner should have all tokens", async () => {
+    expect((await gkToken.balanceOf(owner.address)).toNumber()).to.equal(1000);
+  });
+  it("transfer - should throw error if sender does not have tokens", async () => {
+    gkToken = await gkToken.connect(firstAccount);
+    await expect((gkToken.transfer(secondAccount.address, 100))).to.be
+      .reverted.revertedWith('You do not have tokens to spend');
+  });
+  it("transfer - should transfer tokens to address", async () => {
+    gkToken = await gkToken.connect(owner);
+    await gkToken.transfer(firstAccount.address, 99);
+    expect((await gkToken.balanceOf(firstAccount.address)).toNumber()).to.equal(99);
+  });
+  it("transfer - should emit event of Transfer", async () => {
+    gkToken = await gkToken.connect(owner);
+    await expect(gkToken.transfer(firstAccount.address, 99))
+      .to.emit(gkToken, 'Transfer')
+      .withArgs(owner.address, firstAccount.address, 99);
+  });
+  it("approve - should transfer tokens to address", async () => {
+    gkToken = await gkToken.connect(owner);
+    await gkToken.approve(firstAccount.address, 99);
+    expect((await gkToken.allowance(owner.address, firstAccount.address)).toNumber()).to.equal(99);
+  });
+  it("approve - should emit event of Approval", async () => {
+    gkToken = await gkToken.connect(owner);
+    await expect(gkToken.approve(firstAccount.address, 99))
+      .to.emit(gkToken, 'Approval')
+      .withArgs(owner.address, firstAccount.address, 99);
+  });
+
+  it("transferFrom - should throw error if sender does not have tokens", async () => {
+    gkToken = await gkToken.connect(owner);
+     await expect((gkToken.transferFrom(firstAccount.address, owner.address, 45))).to.be
+      .reverted;
+  });
+  it("transferFrom - should throw error if sender does not have tokens", async () => {
+    gkToken = await gkToken.connect(owner);    
+     await expect((gkToken.transferFrom(owner.address, firstAccount.address, 45))).to.be
+      .revertedWith("From account does not have enough tokens");;
+  });
+  it("Transfer - should transfer the tokens", async () => {
+    gkToken = await gkToken.connect(owner);
+    await gkToken.approve(firstAccount.address, 99);
+    gkToken = await gkToken.connect(firstAccount);
+    await gkToken.transferFrom(owner.address, secondAccount.address, 45)
+    expect((await gkToken.balanceOf(secondAccount.address)).toNumber()).to.equal(45);
+  });
+  it("Transfer - should emit event of Transfer", async () => {
+    gkToken = await gkToken.connect(owner);
+    await gkToken.approve(firstAccount.address, 99);
+    gkToken = await gkToken.connect(firstAccount);
+    await expect(gkToken.transferFrom(owner.address, secondAccount.address, 45))
+    .to.emit(gkToken, 'Transfer')
+    .withArgs(firstAccount.address, secondAccount.address, 45);
+  });
+  it("Transfer - to discount from allowance", async () => {
+    gkToken = await gkToken.connect(owner);
+    await gkToken.approve(firstAccount.address, 99);
+    gkToken = await gkToken.connect(firstAccount);
+    await gkToken.transferFrom(owner.address, secondAccount.address, 45);
+    expect((await gkToken.allowance(owner.address, firstAccount.address)).toNumber()).to.equal(54);
+  });
+});
